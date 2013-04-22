1512fcf9ed0f0daa1ad316d92a4f87c845a5e285
Sara Alspaugh <saraalspaugh@gmail.com>
2013-04-22 14:11:55 -0700

    Changed scripts to only load those commands, args, and templates associated with unique queries.

--- scripts/load_templates.py
+++ scripts/load_templates.py
@@ -10,21 +10,14 @@ BYTES_IN_MB = 1048576
 
 def main():
     done = 0
-    #seen = defaultdict(int)
-    last_query = ""
     db = connect_db()
-    select_cursor = db.execute("SELECT id, text FROM queries WHERE id>?", [177189])
+    #select_cursor = db.execute("SELECT id, text FROM queries WHERE id>?", [273347])
+    select_cursor = db.execute("SELECT min(id), text FROM queries WHERE id>? GROUP BY text", [0])
     for (id, query) in select_cursor.fetchall():
         done += 1
-        if done % 100 == 0:
+        if done % 10 == 0:
             sys.stderr.write(str(done) + " done\n")
             sys.stderr.flush()
-        if query == last_query and last_failed:
-            last_query = query
-            continue
-        else:
-            last_query = query
-            last_failed = False
         check_cursor = db.execute("SELECT * FROM templates \
                                     WHERE query_id=?", [str(id)])
         if len(check_cursor.fetchall()) == 0:
@@ -38,14 +31,6 @@ def main():
             insert_cursor.execute("INSERT INTO templates (query_id, template) \
                             VALUES (?,?)", [id, s])
             db.commit()
-            #seen[s] += 1 
-
-    #print "Number of templates: ", len(seen.keys())
-    #templates = sorted(seen.items(), key=lambda x: x[1], reverse=True)
-    #for (template, count) in templates:
-    #    print "Template: \n", template
-    #    print "Count: ", count
-    #    print
     db.close()
 
 main()
